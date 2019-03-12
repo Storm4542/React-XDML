f184decab13d32de932a6cc6d71183d203cb04a2
Storm4542 <417002107@qq.com>
2019-03-12 16:45:51 +0800

    html-webpack-plugin 配置 index.html 新建

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,3 @@
-console.log('hi111');
+const div = document.createElement('div');
+div.innerText = '你好';
+document.body.appendChild(div);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // html插件，在index.html中引入 js
 module.exports = {
     mode: 'production', // development || production
     entry: {  //入口文件 index.tsx
@@ -6,15 +7,20 @@ module.exports = {
     },
     output: { //输出目录
         path: path.resolve(__dirname, 'dist/lib'), //解决 win（\） 与 mac (/)的目录不同问题
-        library: 'StormUI', //输出库的名字
+        library: 'StormUI', //输出库的名字 可以显示在index.html 的 title 标签中 <title><%= htmlWebpackPlugin.options.title %></title>
         libraryTarget: 'umd', //库的输出格式 umd || amd(requires) || commonjs
     },
     module: { //配置Loader
         rules: [
             {
                 test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader'
+                loader: 'awesome-typescript-loader'  // TS 转换器
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ // html插件，在index.html中引入 js
+            template: 'index.html'
+        })
+    ]
 };
