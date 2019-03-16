ebdea09a625dfed8663a603eb848edc564dc911a
Storm4542 <417002107@qq.com>
2019-03-16 15:56:14 +0800

    icon-1

---
+++ lib/icon.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+function Icon() {
+    return (
+        <span>icon</span>
+    );
+}
+
+export default Icon;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // html插件，在index.html中引入 js
 module.exports = {
     entry: {  //入口文件 index.tsx
         index: './lib/index.tsx'
@@ -20,23 +19,4 @@ module.exports = {
             }
         ]
     },
-    // externals: { //声明为外部依赖
-    //     react: {
-    //         commonjs: 'react', // const react = require('react') 如何引用，配后面的名字
-    //         commonjs2: 'react', //node.js 标准
-    //         amd: 'react', // 浏览器标准
-    //         root: 'React', // <script src = 'xxx/react.js'>  window.React
-    //     },
-    //     'react-dom': {
-    //         commonjs: 'react-dom',
-    //         commonjs2: 'react-dom',
-    //         amd: 'react-dom',
-    //         root: 'ReactDOM',
-    //     },
-    // },
-    // plugins: [
-    //     new HtmlWebpackPlugin({ // html插件，在index.html中引入 js
-    //         template: 'index.html'
-    //     })
-    // ]
 };
