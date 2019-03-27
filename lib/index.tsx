2d1d5a4d6d414873da2744b74ef8b69515881875
Storm4542 <417002107@qq.com>
2019-03-27 16:51:24 +0800

    try publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,12 +1,14 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Icon from './icon/icon';
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+// import Icon from './icon/icon';
 
-const fn: React.MouseEventHandler = (e) => {
-    console.log(e.target);
-};
-ReactDOM.render(<div>
-        <Icon name="wechat" className="qqq" onClick={fn} onMouseEnter={fn} onMouseLeave={fn}/>
-       </div>,
-    document.getElementById('root')
-);
+// const fn: React.MouseEventHandler = (e) => {
+//     console.log(e.target);
+// };
+// ReactDOM.render(<div>
+//         <Icon name="wechat" className="qqq" onClick={fn} onMouseEnter={fn} onMouseLeave={fn}/>
+//        </div>,
+//     document.getElementById('root')
+// );
+
+export {default as Icon} from './icon/icon';
