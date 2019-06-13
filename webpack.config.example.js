e0161d3585bb7b6167eeba72831f39b9b24b9a1b
Storm4542 <417002107@qq.com>
2019-06-13 16:29:06 +0800

    新增 doc 配置 yarn doc

---
+++ webpack.config.example.js
@@ -0,0 +1,18 @@
+const base = require('./webpack.config');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = Object.assign({}, base, {
+    mode: 'production',
+    entry: {
+        example: './example.tsx',
+    },
+    output: { //输出目录
+        path: path.resolve(__dirname, 'doc'), //解决 win（\） 与 mac (/)的目录不同问题
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'example.html',
+            filename: 'example.html'
+        })
+    ],
+});
